f19ea807bbb437305c62b6e32ad187b9249aedfa
lil-doudou <67382584+lil-doudou@users.noreply.github.com>
2023-05-29 14:58:10 +0200

    add nmap output

--- autorecon.sh
+++ autorecon.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+function cleanup() {
+    trap - SIGINT SIGTERM ERR EXIT
+    [[ -z ${TEMP_FOLDER} ]] || rm -rf ${TEMP_FOLDER}
+}
+
 function nmap_recon() {
     local TARGET=$1
-    docker run -it --rm autorecon_nmap ${TARGET}
+    echo "Starting NMAP..."
+    NMAP=$(docker run -it --rm -v ${TEMP_FOLDER}:/opt autorecon_nmap ${TARGET} -oG /opt/nmap_out.txt)
 }
+
+trap cleanup SIGINT SIGTERM ERR EXIT
 
 while [ $# -gt 0 ]; do
     case $1 in
@@ -15,4 +23,7 @@ while [ $# -gt 0 ]; do
     shift
 done
 
+TEMP_FOLDER=$(mktemp -d)
+
 nmap_recon "$TARGET"
+cat ${TEMP_FOLDER}/nmap_out.txt
