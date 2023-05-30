c4e37a5c67e7c64726208c1281bd95360b5326b9
lil-doudou <67382584+lil-doudou@users.noreply.github.com>
2023-05-30 11:23:15 +0200

    WIP: add dirb background docker

--- autorecon.sh
+++ autorecon.sh
@@ -8,7 +8,21 @@ function cleanup() {
 function nmap_recon() {
     local TARGET=$1
     echo "Starting NMAP..."
-    NMAP=$(docker run -it --rm -v ${TEMP_FOLDER}:/opt autorecon_nmap ${TARGET} -oG /opt/nmap_out.txt)
+    NMAP=$(docker run -it --rm -v ${TEMP_FOLDER}:/opt --name autorecon_nmap autorecon_nmap ${TARGET} -oX /opt/nmap_out.txt)
+}
+
+function dirb_recon() {
+    local TARGET=$1
+    local PORT=$2
+    echo "Starting dirb..."
+    if [[ ${PORT} == "80" ]]; then
+        mkfifo ${TEMP_FOLDER}/dirb_80.txt
+        DIRB=$(docker run -it -d --rm -v ${TEMP_FOLDER}:/opt --name autorecon_dirb autorecon_dirb "http://${TARGET}/ -S 2>/opt/dirb_80.txt")
+    elif [[ ${PORT} == "443" ]]; then
+        mkfifo ${TEMP_FOLDER}/dirb_443.txt
+        DIRB=$(docker run -it -d --rm -v ${TEMP_FOLDER}:/opt --name autorecon_dirb autorecon_dirb https://${TARGET})
+    fi
+    echo ${DIRB}
 }
 
 trap cleanup SIGINT SIGTERM ERR EXIT
@@ -26,4 +40,16 @@ done
 TEMP_FOLDER=$(mktemp -d)
 
 nmap_recon "$TARGET"
-cat ${TEMP_FOLDER}/nmap_out.txt
+
+OPEN_PORT=($(awk -F 'portid="' 'match($0, /portid="([0-9]+)"/, m) {print m[1]}' ${TEMP_FOLDER}/nmap_out.txt))
+echo "port open : ${OPEN_PORT[@]}"
+
+for port in ${!OPEN_PORT[@]}; do
+    if [[ ${OPEN_PORT[$port]} == "80" ]] || [[ ${OPEN_PORT[$port]} == "443" ]]; then
+        dirb_recon "${TARGET}" "${OPEN_PORT[$port]}"
+    fi 
+done
+
+while [[ *"$(docker ps -a)"* ==  *"autorecon_"* ]]; do
+    sleep 3
+done
